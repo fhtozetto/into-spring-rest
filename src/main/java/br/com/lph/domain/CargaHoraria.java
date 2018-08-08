87a2f16ac276bbdc100b302d3354f946b5d1c57f
Fernando Henrique Tozetto <fhtozetto@gmail.com>
2018-08-07 21:47:07 -0300

    Classe de Domínio

---
+++ src/main/java/br/com/lph/domain/CargaHoraria.java
@@ -0,0 +1,19 @@
+package br.com.lph.domain;
+
+public enum CargaHoraria {
+	VINTE_HORAS("20HS"),
+    TRINTA_HORAS("30HS"),
+    QUARENTA_HORAS("40HS"),
+    SESSENTA_HORAS("60HS");
+  
+    private String horas;
+  
+    CargaHoraria(String horas) {
+        this.horas = horas;
+    }
+  
+    public String getHoras() {
+        return horas;
+    }
+
+}
