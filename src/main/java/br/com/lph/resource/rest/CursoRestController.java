6105cdb1ed1f767cf3080a4f0f1ef4f7db1aeb13
Fernando Henrique Tozetto <fhtozetto@gmail.com>
2018-08-12 21:21:45 -0300

    Criando um recurso via POST

--- src/main/java/br/com/lph/resource/rest/CursoRestController.java
+++ src/main/java/br/com/lph/resource/rest/CursoRestController.java
@@ -1,14 +1,19 @@
 package br.com.lph.resource.rest;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.lph.domain.Curso;
 import br.com.lph.service.CursoService;
@@ -22,6 +27,19 @@ import br.com.lph.service.CursoService;
 public class CursoRestController {
 	@Autowired
 	private CursoService service;
+	
+	@PostMapping
+	public ResponseEntity<Void> salvar(@RequestBody Curso curso) {
+		service.save(curso);
+		
+		URI location  = ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(curso.getId())
+				.toUri();
+		
+		return ResponseEntity.created(location).build();
+	}
 
 	@GetMapping
 	@ResponseStatus(HttpStatus.OK)
