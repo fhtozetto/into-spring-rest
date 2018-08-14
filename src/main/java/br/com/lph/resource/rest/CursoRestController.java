4e272c5dd6e474204df34f5bc96a3954818afb98
Fernando Henrique Tozetto <fhtozetto@gmail.com>
2018-08-14 18:55:27 -0300

    Recuperando um registro pelo id

--- src/main/java/br/com/lph/resource/rest/CursoRestController.java
+++ src/main/java/br/com/lph/resource/rest/CursoRestController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,6 +28,12 @@ import br.com.lph.service.CursoService;
 public class CursoRestController {
 	@Autowired
 	private CursoService service;
+	
+	@GetMapping("/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	private Curso getCurso(@PathVariable("id") Long id) {
+		return service.findById(id);
+	}
 	
 	@PostMapping
 	public ResponseEntity<Void> salvar(@RequestBody Curso curso) {
