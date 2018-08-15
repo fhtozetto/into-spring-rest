9a6a53c6c372acbb67b6e47383cc30830b6bc0c9
Fernando Henrique Tozetto <fhtozetto@gmail.com>
2018-08-15 18:18:21 -0300

    Padrão Builder para detalhar os erros

--- src/main/java/br/com/lph/domain/DetalheErro.java
+++ src/main/java/br/com/lph/domain/DetalheErro.java
@@ -2,6 +2,8 @@ package br.com.lph.domain;
 
 import java.io.Serializable;
 
+import org.springframework.http.HttpStatus;
+
 public class DetalheErro implements Serializable{
 	private Integer statusCode;
 	private String statusMessage;
@@ -30,4 +32,47 @@ public class DetalheErro implements Serializable{
 		return path;
 	}
 	
+	
+	
+	public static Builder builder() {
+		return new Builder();
+	}
+	
+	public static class Builder{
+		private DetalheErro erro;
+		
+		Builder() {
+			this.erro = new DetalheErro();
+		}
+		
+		public Builder addStatus(HttpStatus status) {
+			this.erro.statusCode = status.value();
+			this.erro.statusMessage = status.getReasonPhrase();
+			return this;
+		}
+
+        public Builder addHttpMethod(String method) {
+            this.erro.httpMethod = method;
+            return this;
+        }
+
+        public Builder addErro(String erro) {
+            this.erro.erro = erro;
+            return this;
+        }
+
+        public Builder addDetalhe(String detalhe) {
+            this.erro.detalhe = detalhe;
+            return this;
+        }
+
+        public Builder addPath(String path) {
+            this.erro.path = path;
+            return this;
+        }
+
+        public DetalheErro build() {
+            return this.erro;
+        }
+	}
 }
